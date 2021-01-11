93e3a7f236a7b8910db6f393d45b7e0d6f411673
nazmul1964dhaka <haquenazmul.dia@gmail.com>
2021-01-11 17:40:20 +0600

    Day 2

---
+++ Day2/Lesson2.py
@@ -0,0 +1,29 @@
+print('have a nice day ',end='')
+print('good bye ',end='')
+print('see you again')
+
+
+s='welcome to python programing language'
+# # print(s)
+# # print(type(s))
+
+
+# x=101
+# print(x)
+# print(type(x))
+
+
+# y=1.1
+# print(y)
+# print(type(y))
+
+
+p=10
+q=3
+r=p+q
+print('the sum is ',r)
+print('the sum of %s+%s=%s'%(p,q,r))
+
+#the addition of 10 and 3 is 13
+
+print('The addition of %s and %s is %s' % (p,q,r) )
